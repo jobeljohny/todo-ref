8382ec29e3de7f12893af51ae7b253a68fe55e72
Isham Mohamed <mail.to.isham@gmail.com>
2024-10-07 00:58:25 +0530

    added add-todo component

---
+++ src/app/add-todo/add-todo.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-todo',
+  templateUrl: './add-todo.component.html',
+  styleUrl: './add-todo.component.scss'
+})
+export class AddTodoComponent {
+newTodo: any;
+addTodo() {
+throw new Error('Method not implemented.');
+}
+
+}
