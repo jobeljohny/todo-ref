ed627caa2ad44ccab6f9728bc9b6984078b85b03
Isham Mohamed <mail.to.isham@gmail.com>
2024-10-07 02:33:34 +0530

    Added create task component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,10 @@ import { MatCardModule } from '@angular/material/card';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { MatTabsModule } from '@angular/material/tabs';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -26,8 +30,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     FormsModule,
     MatCardModule,
     MatTabsModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatButtonModule,
   ],
-  providers: [],
+  providers: [provideNativeDateAdapter()],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/components/add-todo/add-todo.component.ts
+++ src/app/components/add-todo/add-todo.component.ts
@@ -3,12 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-add-todo',
   templateUrl: './add-todo.component.html',
-  styleUrl: './add-todo.component.scss'
+  styleUrl: './add-todo.component.scss',
 })
 export class AddTodoComponent {
-newTodo: any;
-addTodo() {
-throw new Error('Method not implemented.');
-}
+  taskHeading: string = '';
+  taskDescription: string = '';
+  taskEndDate: Date = new Date();
 
+  createTask() {
+    console.log(this.taskEndDate);
+
+    if (this.taskHeading.trim().length === 0) {
+      return;
+    }
+
+    // Logic to handle the created task can be added here
+    console.log('Task Created:', {
+      heading: this.taskHeading,
+      description: this.taskDescription,
+      endDate: this.taskEndDate,
+    });
+    this.taskHeading = '';
+    this.taskDescription = '';
+  }
 }
